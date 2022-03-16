7790bc71035e81e17660cbca91e3e11ae17a74c4
Git1Heo <92144832+Git1Heo@users.noreply.github.com>
2022-03-16 23:20:31 +0900

    update

---
+++ src/main/java/com/project/polaroid/dto/ChatRoomFormDTO.java
@@ -0,0 +1,27 @@
+package com.project.polaroid.dto;
+
+import com.project.polaroid.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomFormDTO {
+
+    private Long id;
+    private String writer;
+    private String lastMessage;
+    private LocalDateTime time;
+    private MemberEntity member;
+
+    public void makeChatRoomForm(String message, String anotherUser, LocalDateTime time) {
+        this.lastMessage = message;
+        this.writer = anotherUser;
+        this.time = time;
+    }
+
+}
